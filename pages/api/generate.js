79222b2ec0fee70588827782a2792e13b744735e
xwebcreations <79429387+xwebcreations@users.noreply.github.com>
2023-04-05 00:37:34 +1200

    Latest build

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -20,7 +20,7 @@ const generateAction = async (req, res) => {
   console.log(`API: ${basePromptPrefix}${req.body.userInput}\n`)
 
   const baseCompletion = await openai.createCompletion({
-    model: 'gpt-3.5-turbo',
+    model: 'text-davinci-003',
     prompt: `${basePromptPrefix}${req.body.userInput}`,
     temperature: 0.7,
     max_tokens: 1000,
